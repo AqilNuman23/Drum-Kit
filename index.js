5637cecf55163f3f6cbc4bcad5664c6d17faa4e9
Aqil Nu'man <130033381+AqilNuman23@users.noreply.github.com>
2023-09-22 02:05:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,24 @@ const numDrumButton = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numDrumButton; i++) {
     const handleClick = () => {
-        alert("I got clicked!");
+        var audio;
+        if (i === 0) {
+            audio = new Audio('./sounds/tom-1.mp3');
+        } else if (i === 1) {
+            audio = new Audio('./sounds/tom-2.mp3');
+        } else if (i === 2) {
+            audio = new Audio('./sounds/tom-3.mp3');
+        } else if (i === 3) {
+            audio = new Audio('./sounds/tom-4.mp3');
+        } else if (i === 4) {
+            audio = new Audio('./sounds/crash.mp3');
+        } else if (i === 5) {
+            audio = new Audio('./sounds/kick-bass.mp3');
+        } else if (i === 6) {
+            audio = new Audio('./sounds/snare.mp3');
+        } 
+    
+        audio.play();
     }
     document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 };
