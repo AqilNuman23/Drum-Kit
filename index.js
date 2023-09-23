898f6900ff3f96287bcb14891b5629954e0c3999
Aqil Nu'man <130033381+AqilNuman23@users.noreply.github.com>
2023-09-24 00:28:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -33,38 +33,57 @@ for (let i = 0; i < numDrumButton; i++) {
 // });
 
 
+// document.addEventListener("keypress", function(event) {
+//     // Check if the pressed key's code is "KeyW"
+//     if (event.code === "KeyW") {
+//         // Create an Audio object and play the sound file
+//         audio = new Audio('./sounds/tom-1.mp3');
+//         audio.play();
+//     } else if (event.code === "KeyA") {
+//         // Create an Audio object and play the sound file
+//         audio = new Audio('./sounds/tom-2.mp3');
+//         audio.play();
+//     } else if (event.code === "KeyS") {
+//         // Create an Audio object and play the sound file
+//         audio = new Audio('./sounds/tom-3.mp3');
+//         audio.play();
+//     } else if (event.code === "KeyD") {
+//         // Create an Audio object and play the sound file
+//         audio = new Audio('./sounds/tom-4.mp3');
+//         audio.play();
+//     } else if (event.code === "KeyJ") {
+//         // Create an Audio object and play the sound file
+//         audio = new Audio('./sounds/crash.mp3');
+//         audio.play();
+//     } else if (event.code === "KeyK") {
+//         // Create an Audio object and play the sound file
+//         audio = new Audio('./sounds/kick-bass.mp3');
+//         audio.play();
+//     } else if (event.code === "KeyL") {
+//         // Create an Audio object and play the sound file
+//         audio = new Audio('./sounds/snare.mp3');
+//         audio.play();
+//     }
+// });
+
+
+
 document.addEventListener("keypress", function(event) {
-    // Check if the pressed key's code is "KeyW"
-    if (event.code === "KeyW") {
-        // Create an Audio object and play the sound file
-        audio = new Audio('./sounds/tom-1.mp3');
-        audio.play();
-    } else if (event.code === "KeyA") {
-        // Create an Audio object and play the sound file
-        audio = new Audio('./sounds/tom-2.mp3');
-        audio.play();
-    } else if (event.code === "KeyS") {
-        // Create an Audio object and play the sound file
-        audio = new Audio('./sounds/tom-3.mp3');
-        audio.play();
-    } else if (event.code === "KeyD") {
-        // Create an Audio object and play the sound file
-        audio = new Audio('./sounds/tom-4.mp3');
-        audio.play();
-    } else if (event.code === "KeyJ") {
-        // Create an Audio object and play the sound file
-        audio = new Audio('./sounds/crash.mp3');
-        audio.play();
-    } else if (event.code === "KeyK") {
-        // Create an Audio object and play the sound file
-        audio = new Audio('./sounds/kick-bass.mp3');
-        audio.play();
-    } else if (event.code === "KeyL") {
-        // Create an Audio object and play the sound file
-        audio = new Audio('./sounds/snare.mp3');
+    // Define an object that maps key codes to sound file paths
+    const keySoundMap = {
+        KeyW: './sounds/tom-1.mp3',
+        KeyA: './sounds/tom-2.mp3',
+        KeyS: './sounds/tom-3.mp3',
+        KeyD: './sounds/tom-4.mp3',
+        KeyJ: './sounds/crash.mp3',
+        KeyK: './sounds/kick-bass.mp3',
+        KeyL: './sounds/snare.mp3',
+    };
+
+    // Check if the pressed key's code exists in the map
+    if (keySoundMap.hasOwnProperty(event.code)) {
+        // Create an Audio object and play the corresponding sound
+        const audio = new Audio(keySoundMap[event.code]);
         audio.play();
     }
 });
-
-
-
